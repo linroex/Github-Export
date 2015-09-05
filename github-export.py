790e9754b01bcc2297288cf70c1ab23374b32a2d
linroex <linroex@linroex-MacBook-Air.local>
2015-09-05 18:28:55 +0800

    cloneAll done

--- github-export.py
+++ github-export.py
@@ -1,20 +1,29 @@
-import sys
-import git
+import sys, os
+from git.repo.base import Repo
+
+path = os.path.dirname(os.path.realpath(__file__))
 
 def main():
-    
     if sys.argv[1] == 'export':
-        print(getAllGithubUrl())
+        cloneAll()
 
 def cloneAll():
-    pass
+    urls = getAllGithubUrl()
+
+    for url in urls:
+        info = getRepoInfo(url)
+        repo = Repo.clone_from(info['url'], path + "/repo_temp/" + info['name'])
+        print(repo)
 
 def getAllGithubUrl():
     with open("url.txt") as f:
         return f.read().split("\n")
 
-def convertGithubUrl(origin_url):
-    return origin_url + ".git"
+def getRepoInfo(origin_url):
+    return {
+        'url': origin_url + ".git",
+        'name': origin_url.split("/")[-1]
+    }
 
 if __name__ == '__main__':
     main()
